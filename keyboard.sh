85ac695cd7c4012737f00e3f5181fd12f3b018c4
sinanm89 <sinanm89@gmail.com>
2014-06-10 07:28:33 +0300

    commented on where the files should be at

--- keyboard.sh
+++ keyboard.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-
+# THIS FILE SHOULD BE IN /etc/keyboard.sh
 address="00:1F:20:4D:7D:7E"
 # Simple forloop counter which tries to connect for a minute
 # It should connect within the first 10 seconds.
